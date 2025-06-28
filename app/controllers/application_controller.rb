9cd12c03497460b1395624e871b579f308f8cd21
Naokichi55 <ibosanganba@gmail.com>
2025-06-28 12:47:18 +0900

    add:ユーザー登録機能、ログイン機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  before_action :configure_permitted_parameters, if :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
-
+  
   def configure_permitted_parameters
-    devise_parameters_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
   end
 end
