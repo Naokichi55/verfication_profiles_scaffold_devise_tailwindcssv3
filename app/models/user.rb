f9bdd93a80e466252be80479c1fb07ecc285be2c
Naokichi55 <ibosanganba@gmail.com>
2025-07-01 00:53:11 +0900

    add:profileモデル、userモデルを記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :rackets, dependent: :destroy
+  has_one :profiles, dependent: :destroy
 end
