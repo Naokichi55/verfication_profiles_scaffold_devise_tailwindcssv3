319bf528789c9c82054aa97741d0d46f33ca43fc
Naokichi55 <ibosanganba@gmail.com>
2025-07-06 09:49:47 +0900

    add:プロフィール登録機能を実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,14 +3,22 @@ class ProfilesController < ApplicationController
 		@profile = Profile.new
 	end
 
+  # def index
+  #    profile = Profile.all
+  # end
+
   def create
-    @profile = current_user.profiles.burild(param_profile)
+    @profile = current_user.profile.build(profile_params)
 		if @profile.save
       redirect_to profile_path, status: :unprocessable_entity
 		else
 			flash new[:danger] = "プロフィールの作成に失敗しました"
 			render  :new, status
 		end
+  end
 
+	private
+  def profile_params
+		params.require(:racket).permit(:racket, :history, :play_style, :level, :gender)
   end
 end
